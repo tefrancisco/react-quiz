ab79f8c504d36368c921622601f3f3726ce872f4
tefrancisco <dzfranciscodz@gmail.com>
2025-12-30 13:35:41 -0300

    Moving a state down

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react'
 import QuestionTimer from "./QuestionTimer.jsx"
 import Answers from "./Answers.jsx"
+import QUESTIONS from '../questions.js'
 
-export default function Question({ questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkipAnswer }) {
+export default function Question({ index , onSelectAnswer, onSkipAnswer }) {
+    const [answer, setAnswer] = useState({
+        selectedAnswer: '',
+        isCorrect: null
+    })
+
+    function handleSelectAnswer(answer) {
+        setAnswer({
+            selectedAnswer: answer,
+            isCorrect: null
+        })
+
+        setTimeout(() => {
+            setAnswer({
+            selectedAnswer: answer,
+            isCorrect: QUESTIONS[index].answers[0] === answer
+        })
+
+        setTimeout(() => {
+            onSelectAnswer(answer)
+        }, 2000)
+        }, 1000)
+    }
+
+    let answerState = ''
+
+    if(answer.selectedAnswer && answer.isCorrect !== null) {
+        answerState = answer.isCorrect ? 'correct' : 'wrong'
+    } else if (answer.selectedAnswer) {
+        answerState = 'answered'
+    }
+    
     return (
          <div id='question'>
                 {/* The progress bar component is not reloaded, to solve this we put a key on it */}
@@ -10,12 +43,12 @@ export default function Question({ questionText, answers, onSelectAnswer, select
                     timeout={10000}
                     onTimeout={onSkipAnswer}
                 />
-                <h2>{questionText}</h2>
+                <h2>{QUESTIONS[index].text}</h2>
                <Answers 
-               answers={answers} 
-               selectedAnswer={selectedAnswer} 
+               answers={QUESTIONS[index].answers} 
+               selectedAnswer={answer.selectedAnswer} 
                answerState={answerState} 
-               onSelect={onSelectAnswer}
+               onSelect={handleSelectAnswer}
                />
             </div>
     )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,32 +4,18 @@ import quizCompleteImg from '../assets/quiz-complete.png'
 import Question from './Question.jsx'
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState('')
     const [userAnswers, setUserAnswers] = useState([])
     // Deriving state, if the array has 2 answers, the activeQuestion must be the third one
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1
+    const activeQuestionIndex = userAnswers.length 
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-        setAnswerState('answered')
         setUserAnswers((prevAnswers) => {
             return [...prevAnswers, selectedAnswer]
         })
 
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct')
-            } else {
-                setAnswerState('wrong')
-            }
-
-            setTimeout(() => {
-                setAnswerState('')
-            }, 2000)
-        }, 1000)
-
-    }, [activeQuestionIndex])
+    }, [])
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
@@ -45,10 +31,7 @@ export default function Quiz() {
         <div id="quiz">
            <Question  
            key={activeQuestionIndex}
-           questionText={QUESTIONS[activeQuestionIndex].text}
-           answers={QUESTIONS[activeQuestionIndex].answers}
-           answerState={answerState}
-           selectedAnswer={userAnswers[userAnswers.length -1]}
+           index={activeQuestionIndex}
            onSelectAnswer={handleSelectAnswer}
            onSkipAnswer={handleSkipAnswer}
            />
