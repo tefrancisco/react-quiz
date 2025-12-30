eac046d3a5fa9e3ac6616a710a850e10826e6516
tefrancisco <dzfranciscodz@gmail.com>
2025-12-30 09:00:28 -0300

    Shuffling questions and outputting a screen after all questions are answered

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,36 @@
 import { useState } from 'react'
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
     // Deriving state, if the array has 2 answers, the activeQuestion must be the third one
     const activeQuestionIndex = userAnswers.length
+   
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer])
     }
+
+    if(quizIsComplete) {
+        return <div id='summary'>
+            <img src={quizCompleteImg} alt="Trophy icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    // In the sort method, if you put a negative number, it swap the elements (a, b), and a positive
+    // number mantains the order. In this case below, the number can be both negative or positive.
+    shuffledAnswers.sort(() => Math.random() -0.5)
 
     return (
         <div id="quiz">
             <div id='question'>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id='answers'>
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className="answer">
+                    {shuffledAnswers.map(answer => <li key={answer} className="answer">
                         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                     </li>)}
                 </ul>
