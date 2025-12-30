3a839027ada805c179deaaf0533d915ae5886c50
tefrancisco <dzfranciscodz@gmail.com>
2025-12-30 08:38:01 -0300

    Implementing the basic logic of the Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react'
+import QUESTIONS from '../questions.js'
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionindex] = useState(0)
     const [userAnswers, setUserAnswers] = useState([])
-    
-    return <p>Currently active Question</p>
+    // Deriving state, if the array has 2 answers, the activeQuestion must be the third one
+    const activeQuestionIndex = userAnswers.length
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer])
+    }
+
+    return (
+        <div id="quiz">
+            <div id='question'>
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id='answers'>
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>)}
+                </ul>
+            </div>
+        </div>
+    )
+
 }
