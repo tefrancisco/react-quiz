1ad4d07f6674f9d251f1c0f360e281861fec89a6
tefrancisco <dzfranciscodz@gmail.com>
2025-12-30 09:35:14 -0300

    Creating the QuestionTimer component and implementing the logic of it

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+    const [remainingTime, setRemainingTime] = useState(timeout)
+    
+    // useEffect to avoid redefining the timeout everytime the component renders
+    useEffect(() => {
+        console.log('SETTING TIMEOOUT')
+        const timer = setTimeout(onTimeout, timeout)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [timeout, onTimeout])
+
+    // useEffect to avoid an infinite loop
+    useEffect(() => {
+        console.log('SETTING INTERVAL')
+        const interval = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 100)
+    }, 100)
+
+    return () => {
+        clearInterval(interval)
+    }
+    }, [])
+
+    return (
+        <progress id="question-time" value={remainingTime} max={timeout}  />
+    )
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from './QuestionTimer.jsx'
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
@@ -9,9 +10,13 @@ export default function Quiz() {
    
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
-    function handleSelectAnswer(selectedAnswer) {
-        setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer])
-    }
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevAnswers) => {
+            return [...prevAnswers, selectedAnswer]
+        } )
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if(quizIsComplete) {
         return <div id='summary'>
@@ -28,6 +33,13 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id='question'>
+                {/* The progress bar component is not reloaded, to solve this we put a key on it */}
+                <QuestionTimer 
+                // Whenever the key changes, React destroy the old component and create a new one
+                key={activeQuestionIndex}
+                timeout={10000} 
+                onTimeout={handleSkipAnswer} 
+                />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id='answers'>
                     {shuffledAnswers.map(answer => <li key={answer} className="answer">
